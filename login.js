53f304c4d2058feadcadabc8ceaf0d254323efba
Abidano2 <77632466+Abidano2@users.noreply.github.com>
2021-02-18 21:51:54 +0000

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+document.querySelector('.button').onclick = function () {
+                var key = document.querySelector('.key').value;
+                function testUrl() {
+                    window.location = "downloads.html";
+                    return false;
+                }
+                if (key == "VThi3x7ccvtmkOAl+guO9OiO7b88DsxmKBQJfDFrb4M=") {
+                    window.location.href = "samdownloads.html"   
+                } else if (key == "jR1IENF8OjYV2Xmqu/uSWAix+q69QhhgkfT+dbBnNcI=") {
+                    window.location.href = "georgedownloads.html"
+                }
+                
+            }
